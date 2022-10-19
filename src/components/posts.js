dc2494fb26ef85f406cc3168446ff27de73d292a
Mahyar Moazezi <59222351+mahyar-moazezi@users.noreply.github.com>
2022-10-18 20:29:42 -0700

    add actions

--- src/components/posts.js
+++ src/components/posts.js
@@ -1,13 +1,24 @@
 import React from "react";
 import { connect } from "react-redux";
+import { deletePost } from "../actions/postActions";
 const PostsComponent = (props) => {
-  const { posts } = props;
-  console.log(posts);
+  const { posts, deletePosts } = props;
+  const clickedOnPost = (id) => {
+    deletePosts(id);
+  };
   return (
     <>
       <div>
         {posts?.map((i) => (
-          <div key={i.id}>
+          <div
+            key={i.id}
+            onClick={() => clickedOnPost(i.id)}
+            style={{
+              border: "1px solid red",
+              margin: "10px",
+              cursor: "pointer"
+            }}
+          >
             <h1>{i.title}</h1>
             <h6>{i.body}</h6>
           </div>
@@ -18,7 +29,14 @@ const PostsComponent = (props) => {
 };
 const mapStateToProps = (state) => {
   return {
-    posts: state.posts,
+    posts: state.posts
   };
 };
-export default connect(mapStateToProps)(PostsComponent);
+const mapDispatchToProps = (dispatch) => {
+  return {
+    deletePosts: (id) => {
+      dispatch(deletePost(id));
+    }
+  };
+};
+export default connect(mapStateToProps, mapDispatchToProps)(PostsComponent);
--- src/reducer/reducer.js
+++ src/reducer/reducer.js
@@ -1,10 +1,19 @@
-const initState ={
-    posts:[
-        {id:1, title:"Hi",body:"how are you?"},
-        {id:2, title:"Hello",body:"I'm good"},
-    ]
-}
-const rootReducer  = (state= initState, action)=>{
-    return state
-}
+const initState = {
+  posts: [
+    { id: 1, title: "Hi", body: "how are you?" },
+    { id: 2, title: "Hello", body: "I'm good" }
+  ]
+};
+const rootReducer = (state = initState, action) => {
+  if (action.type === "DELETE_POSTS") {
+    let newPosts = state.posts.filter((i) => {
+      return i.id !== action.id;
+    });
+    return {
+      ...state,
+      posts: newPosts
+    };
+  }
+  return state;
+};
 export default rootReducer;
